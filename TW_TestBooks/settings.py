903ac912daf1bdde4aabc774e25894c03a1897b0
ancientukr <menshakov2017@gmail.com>
2017-09-12 12:49:11 +0300

    host

--- TW_TestBooks/settings.py
+++ TW_TestBooks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8!hum!8zip*p8b&ml(xtn_q&^b4iojp5i+3o()psof9n(9qul9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zefzef17.pythonanywhere.com']
 
 
 # Application definition
